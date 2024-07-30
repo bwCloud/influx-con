dcbf8beeeec4244619ffb5f2a3bc9e645e36bc34
sunshine <sunshine@teacup>
2024-07-30 09:46:51 +0200

    [3]influx_con: library.py: add parse_library(); Allow executing library commands from other functions;

--- influx_con/library.py
+++ influx_con/library.py
@@ -1,5 +1,22 @@
 """Manage the library content."""
-from typing import Dict
+from typing import Dict, List, Tuple
+from influx_con.utils import read_yaml
+from influx_con.idbccclient import quick_query
+
+
+def parse_library(path: str) -> List[dict]:
+    """Parse the statements in the library.
+
+    Args:
+        path: Path to the file.
+
+    Returns: The statements
+    """
+    library = read_yaml(path)
+    library = library['commands']
+    for e, lib_cmd in enumerate(library):
+        lib_cmd['id'] = e + 1
+    return library
 
 
 def print_library(lib_cmds: Dict[str, str]):
@@ -23,19 +40,62 @@ def print_library(lib_cmds: Dict[str, str]):
 def get_library_command(
     lib_cmds: Dict[str, str],
     lib_id: int
-) -> str:
+) -> Tuple[str, str]:
     """Identify the library entry.
 
     Args:
         lib_cmds: List of commands.
         lib_id: Target command id.
 
-    Returns: Target command.
+    Returns: Target caption and command.
 
     """
     cmd = None
     for lib_cmd in lib_cmds:
         if lib_cmd['id'] == lib_id:
             cmd = lib_cmd['cmd']
+            caption = lib_cmd['description']
 
-    return cmd
+    return caption, cmd
+
+
+def show_library(
+    library_file: str
+):
+    """Print the library commands.
+
+    Args:
+        library_file: Path to the library file.
+
+    """
+    lib_cmds = parse_library(library_file)
+    print_library(lib_cmds)
+
+
+def quick_library(
+    library_file: str,
+    config_file: str,
+    profile: str,
+    lib_id: int,
+):
+    """Run a library command.
+
+    Args:
+        library_file: Path to library file.
+        config_file: Patht to config file.
+        profile: Name of the profile.
+        lib_id: ID of the library command.
+
+    """
+    lib_cmds = parse_library(library_file)
+
+    caption, cmd = get_library_command(
+        lib_cmds,
+        lib_id
+    )
+    quick_query(
+        config_file,
+        profile,
+        cmd,
+        caption=caption
+    )
