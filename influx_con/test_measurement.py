a4cdb2b792c4f669d01cacd58f560c10447f1451
sunshine <sunshine@teacup>
2024-07-30 09:46:51 +0200

    [5]influx_con: test_measurement.py: remove print_test_measurement() and drop_test_measurement();

--- influx_con/test_measurement.py
+++ influx_con/test_measurement.py
@@ -2,11 +2,11 @@
 from influxdb import InfluxDBClient
 from datetime import datetime
 
-from influx_con.idbccclient import run_select_query, print_measurements
-
 
 def insert_test_measurement(client: InfluxDBClient):
     """Insert the test measurements.
+
+    TODO: Write this as an insert function, accessible via CLI.
 
     Args:
         client: Open write connection.
@@ -43,47 +43,3 @@ def insert_test_measurement(client: InfluxDBClient):
 
     client.write_points(meas_entry_1)
     client.write_points(meas_entry_2)
-
-
-def print_test_measurement(client: InfluxDBClient):
-    """Read and dump the test measurements.
-
-    Args:
-        client: Open read connection.
-
-    """
-    meas_name = 'test_meas'
-
-    # Example for getting all values
-    cmd = f'select * from {meas_name};'
-    result = run_select_query(
-        client,
-        cmd
-    )
-    caption1 = f'Result: {meas_name}'
-    print_measurements(
-        result,
-        caption=caption1,
-    )
-
-    # Example for filtering by tags
-    tag_filter2 = {'host': 'second_host'}
-    caption2 = f'Result: {meas_name} for \'second_tag\''
-    print_measurements(
-        result,
-        tag_filter=tag_filter2,
-        caption=caption2,
-    )
-
-
-def drop_test_measurement(client: InfluxDBClient):
-    """Remove the test measurement.
-
-    Args:
-        client: Open admin connection.
-
-    """
-    meas_name = 'test_meas'
-
-    cmd = f'DROP MEASUREMENT "{meas_name}";'
-    client.query(cmd)
